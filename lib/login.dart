9cae142e4ce48ac6e6e0f5a2fd86dea248e8d3ed
Adrien Maloba <adrienmaloba@gmail.com>
2020-04-06 18:40:31 +0200

    [UI] Change login interface

--- lib/login.dart
+++ lib/login.dart
@@ -26,9 +26,8 @@ class _Login extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
-        width: double.infinity,
-        height: double.infinity,
         decoration: BoxDecoration(
             gradient: LinearGradient(
                 begin: Alignment.topCenter,
@@ -40,127 +39,128 @@ class _Login extends State<Login> {
             )
         ),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 80,),
+            SizedBox(height: 100,),
             Padding(
-              padding: EdgeInsets.all(20),
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  FadeAnimation(1, Text("Connexion", style: TextStyle(color: Colors.white, fontSize: 40),)),
-                  SizedBox(height: 10,),
-                  FadeAnimation(1.3, Text("Bienvenue", style: TextStyle(color: Colors.white, fontSize: 18),)),
-                ],
-              ),
-            ),
-            SizedBox(height: 20,),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 60,),
-                      FadeAnimation(1.4, Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [BoxShadow(
-                                color: Color.fromRGBO(225, 95, 27, .3),
-                                blurRadius: 20,
-                                offset: Offset(0, 10)
-                            )]
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey[200]))
-                              ),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    hintText: "Email ou téléphone",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey[200]))
-                              ),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    hintText: "Mot de passe",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      )),
-                      SizedBox(height: 40,),
-                      FadeAnimation(1.5, Text("Mot de passe oublié?", style: TextStyle(color: Colors.grey),)),
-                      SizedBox(height: 40,),
-                      FadeAnimation(1.6, Container(
-                        height: 50,
-                        margin: EdgeInsets.symmetric(horizontal: 50),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: Colors.orange[900]
-                        ),
-                        child: Center(
-                          child: Text("Connexion", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                        ),
-                      )),
-                      SizedBox(height: 50,),
-                      FadeAnimation(1.7, Text("Continuez avec les réseaux sociaux", style: TextStyle(color: Colors.grey),)),
-                      SizedBox(height: 30,),
-                      Row(
+                  FadeAnimation(
+                    1,
+                    Text(
+                      "Bonjour, \nbienvenue",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  FadeAnimation(
+                    1,
+                    Container(
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.transparent,
+                      ),
+                      child: Column(
                         children: <Widget>[
-                          Expanded(
-                            child: FadeAnimation(1.8, Container(
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.blue
+                          Container(
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  color: Colors.grey[100],
+                                ),
                               ),
-                              child: Center(
-                                child: Text("Facebook", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Email ou téléphone",
+                                hintStyle: TextStyle(color: Colors.white),
                               ),
-                            )),
+                            ),
                           ),
-                          SizedBox(width: 30,),
-                          Expanded(
-                            child: FadeAnimation(1.9, Container(
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.black
+                          Container(
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  color: Colors.white,
+                                ),
                               ),
-                              child: Center(
-                                child: Text("Google", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                              ),
-                            )),
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Mot de passe",
+                                  hintStyle: TextStyle(color: Colors.white)),
+                            ),
                           )
                         ],
-                      )
-                    ],
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Center(
+                    child: FadeAnimation(
+                      1,
+                      Text(
+                        "Mot de passe oublié?",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  FadeAnimation(
+                    1,
+                    Container(
+                      height: 50,
+                      margin: EdgeInsets.symmetric(horizontal: 60),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Color.fromRGBO(49, 39, 79, 1),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Connexion",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  FadeAnimation(
+                    1,
+                    Center(
+                      child: Text(
+                        "Créer un compte",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             )
           ],
         ),
-      ),
+      )
     );
   }
 }
